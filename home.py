fec091a8cd4f329379ed91dd51c44dbf29878aa5
sogo <sogonishi@gmail.com>
2024-06-17 20:25:58 +0900

    素材の追加

--- home.py
+++ home.py
@@ -10,14 +10,15 @@ def home(window_name, window_size, title_image):
     start_time = time.time()
 
     # 背景画像の元となる画像を生成
-    background_base = make_background()
+    rand = np.random.randint(0, 5)
+    background_base = make_background(rand)
 
     while True:
         # 現在時刻を取得
         now = time.time()
 
         # 背景画像の生成
-        background_image = animate_background(background_base, window_size, (now - start_time))
+        background_image = animate_background(background_base, rand, window_size, (now - start_time))
 
         press_any_key_image = cv2.imread("src/press_any_key_to_start.png", cv2.IMREAD_UNCHANGED)
         press_any_key_image = image_utils.resize(press_any_key_image, 0.8)
@@ -28,14 +29,14 @@ def home(window_name, window_size, title_image):
         cv2.imshow(window_name, background_image)
 
         # ESCが押されたら画面を切り替える
-        if cv2.waitKey(1) & 0xFF == 27:
+        if cv2.waitKey(1) == 13:
             break
 
 
 # 10x6のグリッドにイカを配置した背景画像の元となる画像を生成
-def make_background():
-    base = cv2.imread("src/background/base/0.png")
-    squid = cv2.imread("src/background/squid/0.png", cv2.IMREAD_UNCHANGED)
+def make_background(rand):
+    base = cv2.imread(f'src/background/base/{rand}.png')
+    squid = cv2.imread(f'src/background/squid/{rand}.png', cv2.IMREAD_UNCHANGED)
 
     base_height = base.shape[0]
     base_width = base.shape[1]
@@ -55,15 +56,15 @@ def make_background():
 
 # 背景画像をアニメーションさせて、装飾を施し、リサイズして返す
 # cycle_timeで指定された時間の周期でアニメーションする
-def animate_background(base, window_size, diff):
+def animate_background(base, rand, window_size, diff):
     cycle_time = 60
     vartical_shift = -(diff % cycle_time) * base.shape[0]/ cycle_time
     horizontal_shift = (diff % cycle_time) * base.shape[1]/ cycle_time
 
-    base = np.roll(base, int(vartical_shift), axis=0)
+    base = np.roll(base, int(-horizontal_shift), axis=0)
     base = np.roll(base, int(horizontal_shift), axis=1)
 
-    mask = cv2.imread("src/background/mask/0.png", cv2.IMREAD_UNCHANGED)
+    mask = cv2.imread(f'src/background/mask/{rand}.png', cv2.IMREAD_UNCHANGED)
     
     image_utils.put_image(base, mask, (int(base.shape[1]/2), int(base.shape[0]/2)))
     base = cv2.resize(base, window_size)
