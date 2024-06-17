43a99b8ba2721e1d5518d0d1275574811db4a7dc
sogo <sogonishi@gmail.com>
2024-06-17 17:45:12 +0900

    background()の改善

--- home.py
+++ home.py
@@ -4,15 +4,15 @@ import utils.image_utils as image_utils
 
 # ホーム画面
 def home(window_name, window_size, title_image):
-    home_image = background(window_size)
+    background = background(window_size)
     press_any_key_image = cv2.imread("src/press_any_key_to_start.png", cv2.IMREAD_UNCHANGED)
     press_any_key_image = image_utils.resize(press_any_key_image, 0.8)
 
-    image_utils.put_image(home_image, title_image, (int(window_size[0]/2), int(window_size[1]/2) - 100))
-    image_utils.put_image(home_image, press_any_key_image, (int(window_size[0]/2), int(window_size[1]/2) + 150))
+    image_utils.put_image(background, title_image, (int(window_size[0]/2), int(window_size[1]/2) - 100))
+    image_utils.put_image(background, press_any_key_image, (int(window_size[0]/2), int(window_size[1]/2) + 150))
 
     while True:
-        cv2.imshow(window_name, home_image)
+        cv2.imshow(window_name, background)
 
         # 任意のキーが押されたら画面を切り替える
         if cv2.waitKey(0) & 0xFF:
@@ -20,11 +20,10 @@ def home(window_name, window_size, title_image):
 
 def background(window_size):
     background = cv2.imread("src/background/0/squid.png")
-    background = cv2.resize(background, window_size)
-    # squid = cv2.imread("src/background/0/mask.png", cv2.IMREAD_UNCHANGED)
-    # squid = cv2.resize(squid, window_size)
+    mask = cv2.imread("src/background/0/mask.png", cv2.IMREAD_UNCHANGED)
     
-    # image_utils.put_image(background, squid, (int(window_size[0]/2), int(window_size[1]/2)))
+    image_utils.put_image(background, mask, (int(background.shape[1]/2), int(background.shape[0]/2)))
+    background = cv2.resize(background, window_size)
 
     return background
 
